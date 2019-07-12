0f6ad40dbf33afe7b0759b5f63259653bbca02d4
Adam Fontenot <adam.m.fontenot@gmail.com>
2019-07-11 20:00:35 -0700

    filter instantly and cheaply using MutationObserver

--- twitch.tv/filter-twitch-chat.user.js
+++ twitch.tv/filter-twitch-chat.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Filter Twitch chat
 // @namespace    https://github.com/afontenot/userjs/twitch-1
-// @version      0.1.0
+// @version      0.2.0
 // @description  Removes most annoying messages on Twitch
 // @author       Adam Fontenot (https://github.com/afontenot)
 // @license      GPL-3.0-or-later
 // @match        https://www.twitch.tv/*
 // @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
@@ -16,31 +17,37 @@
         "peepos", "pogu", "lsrs", "feelsbadman", "poggers", "pogchamp",
         "sourpls", "feelsgoodman", "pepega"
     ];
-
-    setInterval(function() {
-        var messages = document.querySelectorAll('.chat-line__message');
-        for (const message of messages) {
-            var node = message.querySelector('span[data-a-target="chat-message-text"]');
-            if (message.style.display == "none") {
-                continue
+  
+    var filterMessages = function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type !== 'childList' || !mutation.addedNodes.length) {
+                //console.log("unexpected mutation: " + mutation)
+                continue;
             }
-            var msg = node ? node.innerText : null;
-
-            if (!node ||
-                (msg.toUpperCase() == msg && msg.length > 6) ||
-                msg.match(/[A-Z0-9a-z]/) == null) {
-                console.log("filtered " + msg);
-                message.style.display = "none";
-                break;
-            }
-
-            for (const banned_word of banned_words) {
-                if (msg.toLowerCase().includes(banned_word)) {
+            for (const message of mutation.addedNodes) {
+                var node = message.querySelector('span[data-a-target="chat-message-text"]');
+                var msg = node ? node.innerText : null;
+                if (!node ||
+                    (msg.toUpperCase() == msg && msg.length > 6) ||
+                    msg.match(/[A-Z0-9a-z]/) == null) {
                     console.log("filtered " + msg);
                     message.style.display = "none";
                     break;
                 }
+                for (const banned_word of banned_words) {
+                    if (msg.toLowerCase().includes(banned_word)) {
+                        console.log("filtered " + msg);
+                        message.style.display = "none";
+                        break;
+                    }
+                }
             }
         }
-    }, 100);
+    };
+  
+    var chatElement = document.getElementsByClassName("chat-list__lines")[0].querySelector("div[role='log']");
+    var moConfig = { attributes: false, childList: true, subtree: false };
+    var observer = new MutationObserver(filterMessages);
+    observer.observe(chatElement, moConfig);
 })();
+
