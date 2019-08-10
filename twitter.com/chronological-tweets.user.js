926ba0d51fb9c8359db74cadd87dccb7beed89d0
Adam Fontenot <adam.m.fontenot@gmail.com>
2019-08-09 21:06:54 -0700

    fix after twitter changes to css classes

--- twitter.com/chronological-tweets.user.js
+++ twitter.com/chronological-tweets.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Chronological Tweets
-// @version  0.0.1
+// @version  0.0.2
 // @namespace https://github.com/afontenot/userjs/twitter-1
 // @description Automatically clicks the latest tweets mode switch button for you
 // @author       Adam Fontenot (https://github.com/afontenot)
@@ -20,7 +20,7 @@
         var x = document.querySelectorAll('div[aria-label="Top Tweets on"]')
         if (x.length == 1) {
             x[0].click();
-            var y = document.querySelectorAll("div.r-1loqt21.r-18u37iz.r-779j7e.r-23eiwj.r-o7ynqc.r-1j63xyz.r-13qz1uu")[0]
+            var y = document.querySelectorAll("div.r-1loqt21.r-18u37iz.r-o7ynqc.r-1j63xyz.r-13qz1uu")[0]
             y.click();
         }
     }, 1000);
