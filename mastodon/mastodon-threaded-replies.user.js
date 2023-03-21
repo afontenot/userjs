c5eda51b33521d241b80eb4e61d3cf5fdf6b2855
Adam Fontenot <adam.m.fontenot@gmail.com>
2023-03-21 02:54:51 -0400

    move self-replies to their correct threaded location

--- mastodon/mastodon-threaded-replies.user.js
+++ mastodon/mastodon-threaded-replies.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name Mastodon - threaded replies
 // @match https://mastodon.social/*
-// @version 1.5
+// @version 1.6
 // ==/UserScript==
+
+// NOTE: change the match above to your own instance.
 
 /* jshint -W097 */
 'use strict';
 
-// NOTE: change the URL below and the match above to your own instance.
-const instanceURL = "https://mastodon.social";
+
+const instanceURL = (new URL(window.location)).origin;
 const maxIndent = 15;
 
 let loc = window.location.toString();
-let json = {};
+
 let replyMap = {};
 
 const clonableButton = document.createElement("div");
@@ -102,18 +104,23 @@ const addToggleButton = function(reply) {
   reply.parentElement.appendChild(button);
 };
 
-const indentReplies = function() {
-  const replies = document.getElementsByClassName("status-reply");
+const indentReplies = function(json) {
+  const replyElements = document.getElementsByClassName("status-reply");
   // race condition avoidance: try again if not all posts are loaded yet
   // FIXME: could this be flaky if we run while replies are still being added?
-  if (!replies.length) {
-    setTimeout(indentReplies, 100);
+  if (!replyElements.length) {
+    setTimeout(indentReplies, 100, json);
     return;
   }
+
+  // calculate reply depth
   replyMap = {};
   const replyDepth = {};
   let maxDepth = 0;
-  // FIXME: this assumes children never appear before parents in reply list
+
+  // iterate through replies and build reply map and un-hoist self-replies
+  const topLevelPostId = json.descendants[0].in_reply_to_id;
+  const topLevelAccountId = json.descendants[0].in_reply_to_account_id;
   for (const reply of json.descendants) {
     if (!replyMap.hasOwnProperty(reply.in_reply_to_id)) {
       replyMap[reply.in_reply_to_id] = [reply.id];
@@ -128,19 +135,68 @@ const indentReplies = function() {
     if (depth > maxDepth) {
       maxDepth = depth;
     }
+
+    // move self-replies
+    const myParentId = reply.in_reply_to_id;
+    const myPostDate = new Date(reply.created_at);
+    const myParentDescendants = new Set([myParentId]);
+    if (myParentId === topLevelPostId && reply.account.id === topLevelAccountId) {
+      // store the last scanned reply *before* where our reply should go
+      let lastReplyId = myParentId;
+
+      for (const otherReply of json.descendants) {
+        // if otherReply descends from our parent, track it
+        const otherReplyParentId = otherReply.in_reply_to_id;
+        if (myParentDescendants.has(otherReplyParentId)) {
+          myParentDescendants.add(otherReply.id);
+        }
+
+        // Don't consider self-replies, they will get moved after we do.
+        if (reply.account.id === otherReply.account.id) {
+          if (reply.account.id === otherReply.in_reply_to_account_id) {
+            continue;
+          }
+        }
+
+        // get out if a reply is to us
+        if (reply.id === otherReplyParentId) {
+          break;
+        }
+
+        // get out if a sibling has a later date than us
+        const otherPostDate = new Date(otherReply.created_at);
+        if (myParentId === otherReplyParentId && myPostDate < otherPostDate) {
+          break;
+        }
+
+        // set lastReplyId, which tracks targets to move our reply after,
+        // only when the element descends from the same parent as we do.
+        if (myParentDescendants.has(otherReply.id)) {
+          lastReplyId = otherReply.id;
+        }
+      }
+      if (lastReplyId) {
+        const myElement = document.querySelector(`div.status-reply[data-id="${reply.id}"]`).parentElement.parentElement;
+        const otherElement = document.querySelector(`div.status-reply[data-id="${lastReplyId}"]`).parentElement.parentElement;
+        otherElement.insertAdjacentElement("afterend", myElement);
+      }
+    }
   }
+
   const colorMap = {};
   for (let i = 0; i <= maxDepth; i++) {
     colorMap[i] = getRotatedColor(i, maxDepth);
   }
-  for (const reply of replies) {
-    const replyID = reply.getAttribute("data-id");
+
+  // set color and indentation
+  for (const replyElement of replyElements) {
+    const replyID = replyElement.getAttribute("data-id");
     const depth = replyDepth[replyID];
     if (depth >= 0) {
       // set a maximum depth so that indentation doesn't squish too much
-      reply.parentElement.style.marginLeft = `${maxIndent * Math.min(15, depth)}px`;
-      reply.parentElement.style.borderLeft = `5px solid ${colorMap[depth]}`;
-      addToggleButton(reply);
+      replyElement.parentElement.style.marginLeft = `${maxIndent * Math.min(15, depth)}px`;
+      replyElement.parentElement.style.borderLeft = `5px solid ${colorMap[depth]}`;
+      addToggleButton(replyElement);
     }
   }
 };
@@ -159,8 +215,8 @@ const locationChanged = async function() {
   }
   // same origin, shouldn't cause CORS issues
   const resp = await fetch(`${instanceURL}/api/v1/statuses/${pathParts[2]}/context`);
-  json = await resp.json();
-  indentReplies();
+  const json = await resp.json();
+  indentReplies(json);
 };
 
 const checkLocation = function() {
