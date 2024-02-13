9de1261def6a59d02ab393c48a42f2ad80edafa7
Adam Fontenot <adam.m.fontenot@gmail.com>
2024-02-13 14:25:15 -0500

    Change retry behavior
    The previous retry period (1 sec) is not always long enough to load
    replies, so make it 10 sec instead. We still have the issue where
    the replies have loaded but there are missing replies because one or
    more posts are by a blocked user. When this happens, there will be no
    clear indication that reply loading is done, so we need to detect this
    case.
    
    For now, the solution is bailing out after 100 msec once at least one
    reply has been loaded. Context gets loaded from a single JSON file,
    and it should not be common for page JS to take more than 100 msec to
    fully process this file.

--- mastodon/mastodon-threaded-replies.user.js
+++ mastodon/mastodon-threaded-replies.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Mastodon - threaded replies
 // @match https://mastodon.social/*
-// @version 2.1.3
+// @version 2.1.4
 // ==/UserScript==
 
 // NOTE: change the match above to your own instance.
@@ -113,7 +113,12 @@ const indentReplies = function(json) {
   // race condition avoidance: try again if not all posts are loaded yet
   if (replyElements.length < json.ancestors.length + json.descendants.length - 1) {
     fails++;
-    if (fails < 10) {
+    if (fails < 100) {
+      // limit to one more attempt if there's at least one status loaded
+      const anyReplyWithId = document.querySelector(`div.status-reply[data-id]`);
+      if (anyReplyWithId) {
+        fails = 100;
+      }
       setTimeout(indentReplies, 100, json);
       return;
     }
@@ -272,4 +277,3 @@ if (siteName && siteName.getAttribute("content").startsWith("Mastodon")) {
   mutObs.observe(title, mutConfig);
   locationChanged();
 }
-
